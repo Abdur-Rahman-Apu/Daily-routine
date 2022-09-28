71c3bbe2c8dff0f323642b9bebafc0c5bb5845f9
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2022-09-28 11:22:34 -0700

    install react-bootstrap in this project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Components/Home/Home';
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
+      <Button variant="primary">Primary</Button>
       <Home></Home>
     </div>
   );
